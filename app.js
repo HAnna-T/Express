4541845940977809f73fd0cf88a450db912e158a
Anna Harutyunyan <anuanharutyunyan@gmail.com>
2022-03-11 00:46:34 -0800

    Working with express

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+
+const bodyParser = require('body-parser');
+const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname,'views', '404.html'));
+});
+
+app.listen(5000);
